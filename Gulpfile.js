f8c449acf24e788f8d30199e3ca488d014869efe
ACJ <gitlab@acjs.net>
2021-08-13 16:45:57 +0200

    Load data from JSON file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,31 +1,20 @@
 
+"use strict";
+
 var gulp = require('gulp');
 var twig = require('gulp-twig');
+var data = require('gulp-data');
+var fm = require('front-matter');
+var path = require('path');
+var fs = require('fs');
 
 // Compile Twig templates to HTML
 gulp.task('templates', function() {
     return gulp.src(['src/*.twig', '!src/canvas.html.twig'])
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('data.json'));
+        }))
         .pipe(twig({
-            // TODO: Feed it a file. (JSON? ini? YAML? Something else?)
-            data: {
-                artist: {
-                    location: 'The Hague, Netherlands',
-                    name: 'Alexander Christiaan Jacob',
-                    twitter: 'ACJ',
-                    url: 'https://alexanderchristiaanjacob.com/'
-                },
-                artwork: {
-                    dir: 'ltr',
-                    lang: 'en',
-                    description: 'Blank canvas.',
-                    title: 'Canvas',
-                    themeColor: 'white',
-                    image: 'https://deidee.com/logo.png?str=hetcanvas',
-                    url: './'
-                },
-                body: '<h1>Hallo, wereld!</h1>',
-                gtm: 'UA-6227584-61'
-            },
             extname: false
         }))
         .pipe(gulp.dest('dist')); // output the rendered files to the "dist" directory
